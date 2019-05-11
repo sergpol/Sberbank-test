b59d799e3d12213c5e488306ba65a9fbfd1c29ec
Сергей Полицинский <sergpol20@gmail.com>
2019-05-11 12:04:43 +0300

    Initial Commit

---
+++ Sberbank test/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Sberbank test
+//
+//  Created by Сергей Полицинский on 11/05/2019.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sberbank test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Sberbank test
+//
+//  Created by Сергей Полицинский on 11/05/2019.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
